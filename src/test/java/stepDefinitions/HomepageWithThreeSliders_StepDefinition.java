c3dd451767747a40b36045ed7be26fbfa965ed84
Fethiye <fethiye0623@gmail.com>
2023-07-26 23:59:11 +0200

    config.properties added..

--- src/test/java/stepDefinitions/HomepageWithThreeSliders_StepDefinition.java
+++ src/test/java/stepDefinitions/HomepageWithThreeSliders_StepDefinition.java
@@ -2,8 +2,12 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import pages.Practice_Automation_Testing_Homepage;
 
 public class HomepageWithThreeSliders_StepDefinition {
+
+    Practice_Automation_Testing_Homepage homepage=new Practice_Automation_Testing_Homepage();
+
     @Given("i am on the homepage")
     public void i_am_on_the_homepage() {
 
