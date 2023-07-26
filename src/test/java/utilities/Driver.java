c8379cb4ea1f8e9002116fdea255c5b49fa6198f
Fethiye <fethiye0623@gmail.com>
2023-07-26 22:59:23 +0200

    utilities package added..

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,55 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    // create a private static WebDriver object
+    private static WebDriver driver;
+
+    private Driver() { //constructor Driver
+    }
+
+    // create getDriver method to create and initiate the driver instance
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (ConfigurationReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+            }
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    // create a closeDriver method to close the driver
+    public static void closeDriver() {
+        //   quit the driver id it is pointing chromedriver, firefoxdriver....
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
