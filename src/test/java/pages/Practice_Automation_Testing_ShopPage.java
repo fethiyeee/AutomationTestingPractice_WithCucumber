efc270e6f4b83c129051710e46a6dc038a143af8
Fethiye <fethiye0623@gmail.com>
2023-08-11 22:09:00 +0200

    new web element added..

--- src/test/java/pages/Practice_Automation_Testing_ShopPage.java
+++ src/test/java/pages/Practice_Automation_Testing_ShopPage.java
@@ -6,9 +6,13 @@ import utilities.Driver;
 public class Practice_Automation_Testing_ShopPage {
     By shopButton= By.xpath("//*[@id=\"menu-item-40\"]/a[1]");
 
+    By filterButton=By.xpath("//*[@id=\"woocommerce_price_filter-2\"]/form[1]/div[1]/div[2]/button[1]");
+
 
 
     public void clickShopButton(){
         Driver.getDriver().findElement(shopButton).click();
     }
+
+    public void clickFilterButton(){ Driver.getDriver().findElement(filterButton).click();}
 }
