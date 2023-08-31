835447eb445bad48692116a1925924e22773f329
Fethiye <fethiye0623@gmail.com>
2023-09-01 00:32:46 +0200

    9. Test case updated..

--- src/test/java/pages/Practice_Automation_Testing_ShopPage.java
+++ src/test/java/pages/Practice_Automation_Testing_ShopPage.java
@@ -6,39 +6,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.Driver;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Practice_Automation_Testing_ShopPage {
-    By dismissButton=By.xpath("//div[@id='dismiss-button']");
+    By dismissButton = By.xpath("//div[@id='dismiss-button']");
     By shopButton = By.xpath("//*[@id=\"menu-item-40\"]/a[1]");
     By slider = By.xpath("//*[@id=\"woocommerce_price_filter-2\"]/form[1]/div[1]/div[1]/span[2]");
-    //By WE450=By.xpath("//*[@id=\"woocommerce_price_filter-2\"]/form[1]/div[1]/div[2]/div[1]/span[2]");
     By filterButton = By.xpath("//*[@id=\"woocommerce_price_filter-2\"]/form[1]/div[1]/div[2]/button[1]");
     By productPrices = By.xpath("//span[@class='price']");
-   By products=By.xpath("//a[@class='woocommerce-LoopProduct-link']");
-    //y products=By.xpath("//h3");
-    By productName=By.xpath("//h1");
-    By productParticular=By.xpath("//div[@class='summary entry-summary']");
+    By products = By.xpath("//a[@class='woocommerce-LoopProduct-link']");
 
-    public void clickDismissButton(){
-        Driver.getDriver().findElement(dismissButton).click();
-    }
+    By productName = By.xpath("//h1");
+    By productParticular = By.xpath("//div[@class='summary entry-summary']");
+
+
     public void clickShopButton() {
-//        if (Driver.getDriver().findElement(dismissButton).isDisplayed()){
-//            Driver.getDriver().findElement(dismissButton).click();
-//            Driver.getDriver().findElement(shopButton).click();
-//        }else {
-            Driver.getDriver().findElement(shopButton).click();
-       // }
-
+        Driver.getDriver().findElement(shopButton).click();
     }
 
     public void sliderNavigateto450() {
         Actions actions = new Actions(Driver.getDriver());
 
         actions.dragAndDropBy(Driver.getDriver().findElement(slider), -28, 0).build().perform();
-
     }
 
     public void clickFilterButton() {
@@ -49,7 +40,7 @@ public class Practice_Automation_Testing_ShopPage {
         List<WebElement> productPriceElements = Driver.getDriver().findElements(productPrices);
 
         Driver.scrollDownByJS();
-        Thread.sleep(2000);
+        Driver.waitForVisibility(productPrices,1000);
         Driver.scrollIntoViewJS(productPriceElements.get(0));
 
         Assert.assertTrue(Driver.getDriver().findElement(productPrices).isDisplayed());
@@ -60,18 +51,21 @@ public class Practice_Automation_Testing_ShopPage {
         }
     }
 
-    public void clickProductRandom(){
+    public void clickProductRandom() {
         List<WebElement>productList=Driver.getDriver().findElements(products);
-        Random random = new Random();
-        int randomIndex = random.nextInt(productList.size());
-
-        Driver.waitForClickablility(products,1000);
-
-        productList.get(3).click();
+        if (!productList.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(productList.size());
+            WebElement randomElement = productList.get(randomIndex);
+            Driver.waitForClickablility(randomElement,3000);
+            randomElement.click();
+        } else {
+            System.out.println("product not found..");
+        }
 
     }
 
-    public void productParticularIsVisible(){
+    public void productParticularIsVisible() {
         Assert.assertTrue(Driver.getDriver().findElement(productParticular).getText().contains(Driver.getDriver().findElement(productName).getText()));
     }
 }
--- src/test/java/stepDefinitions/ShopPage_ProductClikable_StepDefinition.java
+++
@@ -1,8 +0,0 @@
-package stepDefinitions;
-
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-public class ShopPage_ProductClikable_StepDefinition{
-
-}
