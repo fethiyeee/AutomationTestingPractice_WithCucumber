77f728a5046a948bb82310cd74c7c084640644ff
Fethiye <fethiye0623@gmail.com>
2023-09-05 00:01:35 +0200

    10. Test case added..

--- src/test/java/pages/Practice_Automation_Testing_ShopPage.java
+++ src/test/java/pages/Practice_Automation_Testing_ShopPage.java
@@ -4,9 +4,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.Driver;
 
 import java.util.List;
